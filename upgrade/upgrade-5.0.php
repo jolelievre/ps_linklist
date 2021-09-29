d0a82bd365d89a556d7cc5b819317fdf270b947a
Krystian Podemski <kpodemski@users.noreply.github.com>
2021-09-29 20:59:45 +0200

    Update upgrade/upgrade-5.0.php

--- upgrade/upgrade-5.0.php
+++ upgrade/upgrade-5.0.php
@@ -26,7 +26,7 @@ function upgrade_module_5_0()
     $result = true;
     $result &= Db::getInstance()->execute('ALTER TABLE `' . _DB_PREFIX_ . 'link_block_shop`  ADD COLUMN `position` int(10) unsigned NOT NULL DEFAULT 0');
     // Delete blockcms data that are not used in previous version of this module
-    $result &=Db::getInstance()->execute('DELETE FROM `' . _DB_PREFIX_ . 'link_block_shop`');
+    $result &= Db::getInstance()->execute('DELETE FROM `' . _DB_PREFIX_ . 'link_block_shop`');
 
     foreach (Shop::getShops(true, null, true) as $shopId) {
         $result &= Db::getInstance()->execute(
