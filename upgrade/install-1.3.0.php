457d962e99936484c9b88a1c907150847eeae44d
Daniel Hlaváček <daniel.hlavacek@hotmail.cz>
2024-01-11 13:17:32 +0100

    Update and rename install-1.3.php to install-1.3.0.php

--- upgrade/install-1.3.0.php
+++ upgrade/install-1.3.0.php
@@ -21,7 +21,7 @@ if (!defined('_PS_VERSION_')) {
     exit;
 }
 
-function upgrade_module_1_3($object)
+function upgrade_module_1_3_0($object)
 {
     return $object->registerHook('actionObjectCmsUpdateAfter') && $object->registerHook('actionObjectCmsDeleteAfter');
 }
